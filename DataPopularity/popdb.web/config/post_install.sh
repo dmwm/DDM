f2ce05ee9936f33dad7e6eeed04be2f33f50aec3
DomenicoGiordano <domenico.giordano@cern.ch>
2014-06-12 15:58:53 +0200

    removed not needed lines

--- DataPopularity/popdb.web/config/post_install.sh
+++ DataPopularity/popdb.web/config/post_install.sh
@@ -10,7 +10,6 @@ DASHBUSER=${DASHBUSER:=apache}
 DASHB_NOTIFICATION=${DASHB_NOTIFICATION:=cms-popdb-alarms@cern.ch}
 
 echo "User that will run the service is $DASHBUSER"
-echo "Port for the UDP listener is ${CMSSW_PORT}"
 echo "EMAIL notification address is ${DASHB_NOTIFICATION}"
 
 #useradd --shell /bin/bash --create-home --home-dir /home/cern cern
