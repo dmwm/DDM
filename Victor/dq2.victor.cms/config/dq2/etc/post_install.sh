072433608c7f951bac008e85f9204aede63e273c
Rene Caspart <rcaspart@cern.ch>
2014-06-23 15:31:41 +0200

    added post-install hook to change owner of configuration files from root to cmspopdb

---
+++ Victor/dq2.victor.cms/config/dq2/etc/post_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function changeOwner(){
+    [ ! -d $1  ] &&  mkdir -p $1 
+    chown -R $DASHBUSER $1 
+    echo 'chown for ' $1
+}
+# This is the user that will own the binaries
+DASHBUSER=${DASHBUSER:=cmspopdb}
+DASHB_NOTIFICATION=${DASHB_NOTIFICATION:=cms-popdb-alarms@cern.ch}
+
+echo "User that will run the service is $DASHBUSER"
+echo "EMAIL notification address is ${DASHB_NOTIFICATION}"
+
+#useradd --shell /bin/bash --create-home --home-dir /home/cern cern
+#/usr/sbin/useraddcern cmspopdb
+
+changeOwner /opt/dq2/etc/dq2.cfg
+changeOwner /opt/dq2/etc/logging.cfg
